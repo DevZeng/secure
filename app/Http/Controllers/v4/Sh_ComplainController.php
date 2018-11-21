cc32cefe3d63bde50cd3b060aeb19c9bce48c90a
zadhr <37108229+zadhr@users.noreply.github.com>
2018-11-21 15:53:27 +0800

    Update Sh_ComplainController.php

--- app/Http/Controllers/v4/Sh_ComplainController.php
+++ app/Http/Controllers/v4/Sh_ComplainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\v4;
 use App\Models\Userinfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class Sh_ComplainController extends Controller
 {
