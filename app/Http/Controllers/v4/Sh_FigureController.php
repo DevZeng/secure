63b732fd2e01dfd5c0d75532263aece3b7e9e201
zadhr <37108229+zadhr@users.noreply.github.com>
2018-11-21 15:53:42 +0800

    Update Sh_FigureController.php

--- app/Http/Controllers/v4/Sh_FigureController.php
+++ app/Http/Controllers/v4/Sh_FigureController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v4;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class Sh_FigureController extends Controller
 {
